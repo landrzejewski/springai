c920a5cd3ba6f0ad25bcebc9b01b8ba7fe03441d
landrzejewski <landrzejewski.poczta@gmail.com>
2025-10-28 08:52:53 +0100

    init

--- src/main/java/pl/training/ai/rag/TrainingController.java
+++ src/main/java/pl/training/ai/rag/TrainingController.java
@@ -1,23 +1,32 @@
 package pl.training.ai.rag;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import pl.training.ai.chat.PromptRequest;
 
 @RestController
 public class TrainingController {
 
     private final ChatClient chatClient;
 
-    public TrainingController(OllamaChatModel chatModel) {
+    public TrainingController(OllamaChatModel chatModel, VectorStore vectorStore) {
         this.chatClient = ChatClient.builder(chatModel)
-             //   .defaultAdvisors(new QuestionA)
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
                 .build();
+    }
+
+    @PostMapping("trainings")
+    public TrainingList trainings(@RequestBody PromptRequest promptRequest) {
+        return chatClient
+                .prompt()
+                .user(promptRequest.message())
+                .call()
+                .entity(TrainingList.class);
     }
 
 }
